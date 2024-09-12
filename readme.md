9bb639c471f351b30c4a82a6317b620855f3bcec
andyfam <andyfam@gmail.com>
2024-09-12 11:20:20 -0600

    add SameTree

--- readme.md
+++ readme.md
@@ -433,3 +433,39 @@ Given the `root` of a binary tree, invert the tree, and return its root.
 **Solutions**
 
 1. use recursive
+
+# Same Tree
+
+Given the roots of two binary trees `p` and `q`, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+**Example 1:**
+
+![](/images/ex1.jpg)
+
+> **Input**:  p = [1,2,3], q = [1,2,3]  
+**Output**: true
+
+**Example 2:**
+
+![](/images/ex2.jpg)
+
+> **Input**:  p = [1,2], q = [1,null,2]  
+**Output**: false
+
+**Example 3:**
+
+![](/images/ex3.jpg)
+
+> **Input**:  p = [1,2,1], q = [1,1,2]  
+**Output**: false
+
+**Constraints**
+
+- The number of nodes in both trees is in the range [0, 100].
+- -10^4 <= Node.val <= 10^4
+
+**Solutions**
+
+1. check the value, left node and right node,  because left and right node are also tree, so use recursive.
---
+++ src/test/java/SameTreeTest.java
@@ -0,0 +1,45 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SameTreeTest {
+    @Test
+    void isSameTree() {
+        TreeNode p = getTree1();
+        TreeNode q = getTree1();
+        assertTrue(new SameTree().isSameTree(p, q));
+
+        TreeNode tree1 = new TreeNode(1);
+        TreeNode tree1_2 = new TreeNode(2);
+        tree1.left = tree1_2;
+
+        TreeNode tree2 = new TreeNode(1);
+        TreeNode tree2_2 = new TreeNode(2);
+        tree2.right = tree2_2;
+        assertFalse(new SameTree().isSameTree(tree1, tree2));
+
+        TreeNode t1 = new TreeNode(1);
+        TreeNode t1_2 = new TreeNode(2);
+        TreeNode t1_3 = new TreeNode(1);
+        t1.left = t1_2;
+        t1.right = t1_3;
+
+        TreeNode t2 = new TreeNode(1);
+        TreeNode t2_2 = new TreeNode(1);
+        TreeNode t2_3 = new TreeNode(2);
+        t2.left = t2_2;
+        t2.right = t2_3;
+        assertFalse(new SameTree().isSameTree(t1, t2));
+    }
+
+    private TreeNode getTree1() {
+        TreeNode tree1 = new TreeNode(1);
+        TreeNode tree2 = new TreeNode(2);
+        TreeNode tree3 = new TreeNode(3);
+        tree1.left = tree2;
+        tree1.right = tree3;
+
+        return tree1;
+    }
+}
