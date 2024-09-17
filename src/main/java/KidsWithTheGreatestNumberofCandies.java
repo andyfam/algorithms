1fdf686cb7949dd0081df2ab405806d1516ea892
andyfam <andyfam@gmail.com>
2024-09-17 15:04:31 -0600

    add KidsWithTheGreatestNumberofCandies

---
+++ src/main/java/KidsWithTheGreatestNumberofCandies.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class KidsWithTheGreatestNumberofCandies {
+
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> results = new ArrayList<>();
+
+        int largest = candies[0];
+        for (int i = 1; i < candies.length; i++) {
+            if (candies[i] > largest) {
+                largest = candies[i];
+            }
+        }
+
+        for (int i = 0; i < candies.length; i++) {
+            if (candies[i] + extraCandies >= largest) {
+                results.add(true);
+            } else {
+                results.add(false);
+            }
+        }
+
+        return results;
+
+    }
+}
---
+++ src/test/java/KidsWithTheGreatestNumberofCandiesTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class KidsWithTheGreatestNumberofCandiesTest {
+    @Test
+    void kidsWithCandies() {
+        assertEquals(Arrays.asList(true, true, true, false, true), new KidsWithTheGreatestNumberofCandies().kidsWithCandies(new int[]{2, 3, 5, 1, 3}, 3));
+        assertEquals(Arrays.asList(true,false,false,false,false), new KidsWithTheGreatestNumberofCandies().kidsWithCandies(new int[]{4,2,1,1,2}, 1));
+        assertEquals(Arrays.asList(true,false,true), new KidsWithTheGreatestNumberofCandies().kidsWithCandies(new int[]{12,1,12}, 10));
+
+    }
+}
